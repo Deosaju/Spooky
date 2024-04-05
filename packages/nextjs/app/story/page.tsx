3bcd34a4ddcc6dd304e5fc4d08428cc736456e2f
Deo saju <deosaju@gmail\.com>
2024-04-06 04:21:20 +0530

    Saving NFT in tableland Done

--- packages/nextjs/app/story/page.tsx
+++ packages/nextjs/app/story/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 import Loader from "../../components/Loader";
 import openai from "../../services/openai/openai";
 import generateStoryPrompt from "./_components/GenerateStoryPrompt";
@@ -17,9 +18,11 @@ const storyContract = new StoryContract();
 interface Message {
   text: string;
   sender: string;
+  image?: string;
 }
 
 const Story: NextPage = () => {
+  const router = useRouter();
   const { address: connectedAddress } = useAccount();
   const [isLoading, setIsLoading] = useState(true);
   const [messages, setMessages] = useState<Message[]>([
@@ -35,6 +38,8 @@ const Story: NextPage = () => {
   const [isBotTyping, setIsBotTyping] = useState(false);
   const chatContainerRef = useRef<HTMLDivElement>(null);
   const [isSaving, setIsSaving] = useState(false);
+  const [title, setTitle] = useState("");
+  const [imageMessage, setImageMessage] = useState<Message | null>(null);
 
   useEffect(() => {
     setTimeout(() => {
@@ -47,6 +52,12 @@ const Story: NextPage = () => {
       chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
     }
   }, [messages]);
+
+  useEffect(() => {
+    if (imageMessage) {
+      setMessages(prevMessages => prevMessages.map(msg => (msg === imageMessage ? imageMessage : msg)));
+    }
+  }, [imageMessage]);
 
   const handleSendMessage = async () => {
     if (inputMessage.trim() !== "") {
@@ -71,6 +82,10 @@ const Story: NextPage = () => {
             setMessages([...messages, userMessage, { text: generatedStory, sender: "Bot" }]);
             setFullStory(fullStory + generatedStory);
             setMessagesStore(messagesStore + generatedStory);
+            const updatedMessage = await generateImageForMessage({ text: generatedStory, sender: "Bot" });
+            if (updatedMessage) {
+              setImageMessage(updatedMessage);
+            }
           }
         } else {
           console.error("Error generating story:", APIConnectionError);
@@ -99,6 +114,22 @@ const Story: NextPage = () => {
     }
   };
 
+  const generateImageForMessage = async (message: Message) => {
+    try {
+      const prompt = await generateImagePrompts(message.text);
+      const image = await generateImagesFromPrompts(prompt);
+
+      const updatedMessage: Message = {
+        ...message,
+        image: image,
+      };
+
+      return updatedMessage;
+    } catch (error) {
+      console.error("Error generating image:", error);
+    }
+  };
+
   const handleSaveStory = async () => {
     setIsSaving(true);
     try {
@@ -106,7 +137,7 @@ const Story: NextPage = () => {
       if (title) {
         await storyContract.saveStory(title, fullStory);
         alert("Story saved successfully!");
-        window.location.href = "http://localhost:3000/stories";
+        router.push(`/stories/stories`);
       } else {
         alert("Title is required to save the story.");
       }
@@ -119,13 +150,31 @@ const Story: NextPage = () => {
 
   const handleMintNFTs = async () => {
     try {
-      const title = prompt("Please enter the title of the story:");
       if (title) {
         const prompt = await generateImagePrompts(fullStory);
-        const generatedImages = await generateImagesFromPrompts(prompt);
-        const storyId = await tablelandUtils.saveStory(title, connectedAddress ? connectedAddress : "Unknown");
-        await tablelandUtils.saveNFTImages(storyId, generatedImages, prompt);
-        window.location.href = `http://localhost:3000/stories/${title}/nfts`;
+        // const generatedImages = await generateImagesFromPrompts(prompt);
+        const existingStory = await tablelandUtils.getStoryByTitle(title);
+        const connectedAuthor = connectedAddress || "Unknown";
+
+        if (existingStory && existingStory[0].author !== connectedAuthor) {
+          alert(`The title "${title}" already exists and is owned by another author. Please choose a different title.`);
+          return;
+        }
+
+        let storyId;
+        if (existingStory) {
+          storyId = existingStory[0].id;
+        } else {
+          storyId = await tablelandUtils.saveStory(title, connectedAddress ? connectedAddress : "Unknown");
+          console.log("This is story id " + storyId);
+        }
+        if (storyId !== null) {
+          console.log(storyId);
+          await tablelandUtils.saveNFTImages(storyId, "Teast", prompt);
+          router.push(`/stories/${title}/nfts`);
+        } else {
+          alert("Failed to save the story. Please try again.");
+        }
       } else {
         alert("Title is required to mint NFTs.");
       }
@@ -134,15 +183,59 @@ const Story: NextPage = () => {
       alert("Error minting NFTs. Please try again.");
     }
   };
+  const handleMintNFT = async (image: string) => {
+    await handleSaveImageToTableland(image);
+  };
+
+  const handleSaveImageToTableland = async (imageUrl: string) => {
+    try {
+      if (title) {
+        const existingStory = await tablelandUtils.getStoryByTitle(title);
+        const connectedAuthor = connectedAddress || "Unknown";
+
+        if (existingStory && existingStory[0].author !== connectedAuthor) {
+          alert(`The title "${title}" already exists and is owned by another author. Please choose a different title.`);
+          return;
+        }
+        let storyId;
+        if (existingStory != null && existingStory.length != 0) {
+          storyId = existingStory[0].id;
+        } else {
+          storyId = await tablelandUtils.saveStory(title, connectedAddress ? connectedAddress : "Unknown");
+          console.log("This is story id " + storyId);
+        }
+        if (storyId !== null) {
+          console.log(storyId);
+          await tablelandUtils.saveNFTImages(storyId, imageUrl, inputMessage);
+        } else {
+          alert("Failed to save the story. Please try again.");
+        }
+      } else {
+        alert("Title is required to mint NFTs.");
+      }
+    } catch (error) {
+      console.error("Error saving image to Tableland:", error);
+    }
+  };
+
   return (
     <>
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="container mx-auto my-10">
-          <h1 className="text-4xl text-center font-bold mb-10">Story Generator</h1>
+        <div className="container mx-auto my-5">
+          <h1 className="text-4xl text-center font-bold mb-5">Story Generator</h1>
+          <label htmlFor="titleInput">Title: </label>
+          <input
+            id="titleInput"
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Enter the story title"
+            className="border border-gray-300 rounded-xl px-4 py-2 mb-4"
+          />
           <div className="border border-gray-300 bg-slate-700 rounded-lg p-4">
-            <div ref={chatContainerRef} className="h-96 overflow-y-auto hide-scrollbar p-10">
+            <div ref={chatContainerRef} className="h-96 overflow-y-auto hide-scrollbar px-10 pt-10">
               {messages.map((message, index) => (
                 <div key={index} className={`mb-2 ${message.sender === "User" ? "text-right" : "text-left"}`}>
                   <span
@@ -152,6 +245,21 @@ const Story: NextPage = () => {
                   >
                     {message.text}
                   </span>
+                  {message.sender === "Bot" && message.image && (
+                    <div className="w-1/2 mx-auto">
+                      <div className="relative">
+                        <img src={message.image} alt={`Generated Image ${index}`} className="w-full" />
+                        <div className="absolute inset-x-0 bottom-2">
+                          <button
+                            onClick={() => message.image && handleMintNFT(message.image)}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto block"
+                          >
+                            Mint NFT
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
               ))}
               {isBotTyping && (
@@ -161,7 +269,23 @@ const Story: NextPage = () => {
                   </span>
                 </div>
               )}
+              {imageMessage && (
+                <div className="w-1/2 mx-auto">
+                  <div className="relative">
+                    <img src={imageMessage.image} alt="Generated Image" className="w-full" />
+                    <div className="absolute inset-x-0 bottom-2">
+                      <button
+                        onClick={() => imageMessage?.image && handleMintNFT(imageMessage.image)}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto block"
+                      >
+                        Mint NFT
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
+
             <div className="mt-4 flex">
               <input
                 type="text"
@@ -192,7 +316,7 @@ const Story: NextPage = () => {
               {isSaving ? "Saving..." : "Save Story"}
             </button>
             <button onClick={handleMintNFTs} className="btn btn-outline rounded-md mt-5 btn-primary">
-              Mint NFTs
+              Mint Random NFT Based On Story
             </button>
           </div>
         </div>
--- packages/nextjs/utils/tableland/tablelandutil.ts
+++ packages/nextjs/utils/tableland/tablelandutil.ts
@@ -9,40 +9,45 @@ export class TablelandUtils {
     this.tablelandService.connect();
   }
 
-  async saveStory(title: string, author: string): Promise<number> {
-    const query = `INSERT INTO ${process.env.NEXT_PUBLIC_TABLELAND_STORY} (title, author) VALUES (?, ?)`;
+  async getStoryByTitle(title: string): Promise<any[] | null> {
+    const query = `SELECT id ,author ,title FROM ${process.env.NEXT_PUBLIC_TABLELAND_STORY} WHERE title = ? LIMIT 1`;
     const statement = this.tablelandService.getDatabase().prepare(query);
-    await statement.bind(title, author).run();
+    const result = await statement.bind(title).run();
 
-    const selectQuery = `SELECT last_insert_rowid() as id`;
+    if (result.success) {
+      return result.results;
+    } else {
+      return null;
+    }
+  }
+
+  async saveStory(title: string, author: string): Promise<any[] | null> {
+    const insertQuery = `INSERT INTO ${process.env.NEXT_PUBLIC_TABLELAND_STORY} (title, author) VALUES (?, ?)`;
+    const insertStatement = this.tablelandService.getDatabase().prepare(insertQuery);
+    await insertStatement.bind(title, author).run();
+
+    const selectQuery = `SELECT MAX(id) as id FROM ${process.env.NEXT_PUBLIC_TABLELAND_STORY}`;
     const selectStatement = this.tablelandService.getDatabase().prepare(selectQuery);
     const result: Result<any> = await selectStatement.run();
 
-    return result.meta.rows[0].id;
+    if (result.success) {
+      return result.results;
+    } else {
+      return null;
+    }
   }
 
   async saveNFTImages(storyId: number, imageUrls: string, prompts: string): Promise<number[]> {
-    const query = `INSERT INTO ${process.env.TABLELAND_PREFIX}_nft_images (story_id, image_url, prompt) VALUES (?, ?, ?)`;
-    const statement = this.tablelandService.getDatabase().prepare(query);
-
-    const insertedIds: number[] = [];
-
-    await statement.bind(storyId, imageUrls, prompts).run();
-
-    const selectQuery = `SELECT last_insert_rowid() as id`;
-    const selectStatement = this.tablelandService.getDatabase().prepare(selectQuery);
-    const result: Result<any> = await selectStatement.run();
-
-    insertedIds.push(result.meta.rows[0].id);
-
-    return insertedIds;
+    const insertQuery = `INSERT INTO ${process.env.NEXT_PUBLIC_TABLELAND_NFT} (story_id, image_url, prompt) VALUES (?, ?, ?)`;
+    const insertStatement = this.tablelandService.getDatabase().prepare(insertQuery);
+    const result: Result<any> = await insertStatement.bind(storyId, imageUrls, prompts).run();
+    return result.meta.rows;
   }
 
   async getNFTImagesForStory(storyId: number): Promise<any[]> {
-    const query = `SELECT * FROM ${process.env.TABLELAND_PREFIX}_nft_images WHERE story_id = ?`;
+    const query = `SELECT * FROM ${process.env.NEXT_PUBLIC_TABLELAND_NFT} WHERE story_id = ?`;
     const statement = this.tablelandService.getDatabase().prepare(query);
     const result: Result<any> = await statement.bind(storyId).run();
-
     return result.meta.rows;
   }
 }
